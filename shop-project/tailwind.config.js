3b0b7ba471d845d3acbd41b5cd48d34ded20b746
Harry Smith <harryawsmith@gmail.com>
2022-10-11 21:16:12 +1100

    initial commit

---
+++ shop-project/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/Pages/**/*.{html,js}',
+    './src/Components/**/*.{html,js}',
+    './public/index.html',
+    './src/**/*.{html,js}',
+    './index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
